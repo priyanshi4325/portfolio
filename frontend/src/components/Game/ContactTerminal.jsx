5173355cbdaff03c5fc13d34043c9e695bfecced
priyanshi4325 <priyanshdass@gmail.com>
2026-03-11 00:34:46 +0530

    commit frontend to render backend

--- frontend/src/components/Game/ContactTerminal.jsx
+++ frontend/src/components/Game/ContactTerminal.jsx
@@ -31,7 +31,7 @@ const ContactTerminal = () => {
     setMessage('ENCRYPTING & SENDING TRANSMISSION...');
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/api/contacts/', {
+      const response = await fetch('https://portfolio-backend-5zvg.onrender.com/api/contacts/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({

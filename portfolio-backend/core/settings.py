d2ad9cd9033e8426745abd827e64971d7d63acc3
priyanshi4325 <priyanshdass@gmail.com>
2026-03-11 00:23:32 +0530

    allowed hosts

--- portfolio-backend/core/settings.py
+++ portfolio-backend/core/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5175",

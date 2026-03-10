58d16bb24401ea42d1c7111ffeb2777341d76562
priyanshi4325 <priyanshdass@gmail.com>
2026-03-11 00:14:51 +0530

    prepared files for render connectivity

--- portfolio-backend/core/settings.py
+++ portfolio-backend/core/settings.py
@@ -1,17 +1,15 @@
 from pathlib import Path
+import dj_database_url
+from decouple import config
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%@0jzl=syzy#bpf%_2)2t9=1+!(og3z=(3cf@3mne%@q(u4km@'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -70,10 +68,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 
